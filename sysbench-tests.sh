35b07986d7d9d697d22e79966eb8a1edae795005
JeffReeves <jeff@binary.run>
2020-10-04 13:35:13 -0700

    Create sysbench test suite
    Created initial test suite starting with CPU tests.
    
    Current tests are 16 and 128 thread cpu tests with 20000 prime numbers.

---
+++ sysbench-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# purpose: runs a suite of sysbench tests to benchmark components
+# author: Jeff Reeves
+
+#==[CPU]=======================================================================
+
+# init
+CPU_MAX_PRIME='20000'
+CPU_THREADS='16 128'
+
+# start tests
+echo "[TASK] Running CPU tests ..."
+echo ''
+
+for THREADS in ${CPU_THREADS}; do
+    echo "[TASK] Start test with ${THREADS} threads ..."
+    sysbench --test=cpu --cpu-max-prime=${CPU_MAX_PRIME} --num-threads=${THREADS} run
+    echo ''
+done
+
+echo "[INFO] CPU tests complete"
+echo ''
+
+#==============================================================================
