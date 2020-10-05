519238408fb09c1fc506c77aceb92084605c5ec1
JeffReeves <jeff@binary.run>
2020-10-04 17:01:39 -0700

    Added memory tests
    Added several memory tests using:
    
    - 16 and 128 threads
    
    - read and write
    
    - random and sequential
    
    Using 100G total size and 200M blocks.

--- sysbench-tests.sh
+++ sysbench-tests.sh
@@ -4,9 +4,15 @@
 
 # general formatting function
 function print_separator {
-    printf '#'
-    printf '=%.0s' {1..79}
-    printf '\n'
+    if [ "${1}" == '-' ]; then
+        printf '#'
+        printf -- '-%.0s' {1..79}
+        printf '\n'
+    else
+        printf '#'
+        printf '=%.0s' {1..79}
+        printf '\n' 
+    fi
 }
 
 #==[CPU]=======================================================================
@@ -31,6 +37,7 @@ for THREADS in ${CPU_THREADS}; do
     --threads=${THREADS} \
     run
     echo ''
+    print_separator -
 done
 
 echo "[END] CPU TESTS"
@@ -39,16 +46,56 @@ echo ''
 
 #==[MEMORY]====================================================================
 
-# # init
+# init
+MEMORY_THREADS='16 128'
+MEMORY_OPERATIONS='read write'
+MEMORY_ACCESS_MODE='rnd seq'
+MEMORY_TOTAL_SIZE='100G'
+MEMORY_BLOCK_SIZE='200M'
 
-# # start tests
-# print_separator
-# echo "[BEGIN] MEMORY TESTS"
-# echo ''
+# start tests
+print_separator
+echo "[BEGIN] MEMORY TESTS"
+echo ''
 
-# echo "[END] MEMORY TESTS"
-# print_separator
-# echo ''
+for THREADS in ${CPU_THREADS}; do
+    echo "[TASK] Start test with ${THREADS} threads ..."
+    echo ''
+    for OPERATION in ${MEMORY_OPERATIONS}; do
+        echo "[TASK] Running test with operation '${OPERATION}' ..."
+        echo ''
+        for MODE in ${MEMORY_ACCESS_MODE}; do
+            echo "[INFO] Running test with:"
+            echo "OPERATION: '${OPERATION}'"
+            echo "MODE:      '${MODE}'"
+            echo ''
+            echo "[COMMAND] sysbench memory \
+            --num-threads=${THREADS} \
+            --memory-total-size=${MEMORY_TOTAL_SIZE} \
+            --memory-block-size=${MEMORY_BLOCK_SIZE} \
+            --memory-scope=global \
+            --memory-hugetlb=off \
+            --memory-oper=${OPERATION} \
+            --memory-access-mode=${MODE} \
+            run"
+            sysbench memory \
+            --num-threads=${THREADS} \
+            --memory-total-size=${MEMORY_TOTAL_SIZE} \
+            --memory-block-size=${MEMORY_BLOCK_SIZE} \
+            --memory-scope=global \
+            --memory-hugetlb=off \
+            --memory-oper=${OPERATION} \
+            --memory-access-mode=${MODE} \
+            run
+            echo ''
+            print_separator -
+        done
+    done
+done
+
+echo "[END] MEMORY TESTS"
+print_separator
+echo ''
 
 #==[DISK I/O]==================================================================
 
@@ -96,6 +143,7 @@ for THREADS in ${FILE_THREADS}; do
         --events=${FILE_MAX_EVENTS} \
         run
         echo ''
+        print_separator -
     done
 done
 
